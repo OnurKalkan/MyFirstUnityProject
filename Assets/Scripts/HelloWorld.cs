67090591826e50f33c2a25cc68f15851f77fbcd5
OnurKalkan <onurkalkanv84@hotmail.com>
2024-03-04 22:59:31 +0300

    Hello World güncellemesi

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    //integers
+    byte levelNo = 255;
+    sbyte withPositive = 127;
+    short smallNo = 32_000;
+    ushort smallNoNoNegative = 64_000;
+    public int speed = 12;
+    uint speed2 = 25;
+    ulong bigNumber = 1_000_000_000_000_000_000;    
+    //decimals
+    float kesirliSayi = 10.5f;
+    double doubleDecimal = 10.7;
+    decimal decNo = 22.5m;
+    //characters
+    string myText = "Hello World 2!!";
+    char smiley = 's';
+    //Unity ozel
+    public GameObject helloText;
+
     // Start is called before the first frame update
     void Start()
     {
-        //inside the method
+        print(myText);//console msji
+        Application.targetFrameRate = 100;//oyunun fpsini sabitleme
+        helloText.GetComponent<TextMeshPro>().text = "Hello World!!";
     }
 
     // Update is called once per frame
     void Update()
     {
-        //inside the update method
+        //Debug.Log("Hello Again");
+        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+        
     }
 }
